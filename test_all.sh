c456666a72f97572fd26741d55b4faa328f9c319
dave <dmh2000@gmail.com>
2023-08-01 11:03:49 -0700

    updated github action to store output

--- test_all.sh
+++ test_all.sh
@@ -1,48 +1,38 @@
 #!/bin/bash
 
-rm results.txt
-echo "Operators" 
-echo "Operators" >>results.txt
-go test -v ./app/operators*.go ./app/test_util.go ./app/app_types.go 2>&1>>results.txt
+echo "" >$GITHUB_OUTPUT
 
-echo "Flow Control"
-echo "Flow Control" >>results.txt
-go test -v ./app/flowControl*.go ./app/test_util.go ./app/app_types.go 2>&1>>results.txt
+echo "Operators"
+go test -v ./app/operators*.go ./app/test_util.go ./app/app_types.go  2>&1>> $GITHUB_OUTPUT
+
+echo "Flow Control" 
+go test -v ./app/flowControl*.go ./app/test_util.go ./app/app_types.go 2>&1>> $GITHUB_OUTPUT
 
 echo "Strings" 
-echo "Strings" >>results.txt
-go test -v ./app/strings*.go  ./app/test_util.go ./app/app_types.go 2>&1>>results.txt
+go test -v ./app/strings*.go  ./app/test_util.go ./app/app_types.go 2>&1>> $GITHUB_OUTPUT
 
 echo "Slices" 
-echo "Slices" >>results.txt
-go test -v ./app/slices*.go  ./app/test_util.go ./app/app_types.go 2>&1>>results.txt
+go test -v ./app/slices*.go  ./app/test_util.go ./app/app_types.go 2>&1>> $GITHUB_OUTPUT
 
 echo "Binary" 
-echo "Binary" >>results.txt
-go test -v ./app/binary*.go ./app/test_util.go ./app/app_types.go 2>&1>>results.txt
+go test -v ./app/binary*.go ./app/test_util.go ./app/app_types.go 2>&1>> $GITHUB_OUTPUT
 
 echo "Functions" 
-echo "Functions" >>results.txt
-go test -v ./app/functions*.go ./app/test_util.go ./app/app_types.go 2>&1>>results.txt
+go test -v ./app/functions*.go ./app/test_util.go ./app/app_types.go 2>&1>> $GITHUB_OUTPUT
 
-echo "Methods"
-echo "Methods" >>results.txt
-go test -v ./app/method*.go  ./app/test_util.go ./app/app_types.go 2>&1>>results.txt
+echo "Methods" 
+go test -v ./app/method*.go  ./app/test_util.go ./app/app_types.go 2>&1>> $GITHUB_OUTPUT
 
-echo "Collections"
-echo "Collections" >>results.txt
-go test -v ./app/collections*.go  ./app/test_util.go ./app/app_types.go 2>&1>>results.txt
+echo "Collections" 
+go test -v ./app/collections*.go  ./app/test_util.go ./app/app_types.go 2>&1>> $GITHUB_OUTPUT
 
-echo "Recursion"
-echo "Recursion" >>results.txt
-go test -v ./app/recursion*.go ./app/test_util.go ./app/app_types.go 2>&1>>results.txt
+echo "Recursion" 
+go test -v ./app/recursion*.go ./app/test_util.go ./app/app_types.go 2>&1>> $GITHUB_OUTPUT
 
-echo "Regexp"
-echo "Regexp" >>results.txt
-go test -v ./app/regex*.go  ./app/test_util.go ./app/app_types.go 2>&1>>results.txt
+echo "Regexp" 
+go test -v ./app/regex*.go  ./app/test_util.go ./app/app_types.go 2>&1>> $GITHUB_OUTPUT
 
-echo Async
-echo "Async" >>results.txt
-go test -v ./app/async*.go  ./app/test_util.go ./app/app_types.go 2>&1>>results.txt
+echo "Async" 
+go test -v ./app/async*.go  ./app/test_util.go ./app/app_types.go 2>&1>> $GITHUB_OUTPUT
 
 exit 0
