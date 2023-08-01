a2adc1de9d512d8a822ea3f50a9e45d26f79e4f7
dave <dmh2000@gmail.com>
2023-08-01 11:14:24 -0700

    updating github workflow again

--- test_all.sh
+++ test_all.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 
-echo "" >$GITHUB_OUTPUT
 
 echo "Operators"
-go test -v ./app/operators*.go ./app/test_util.go ./app/app_types.go  2>&1>> $GITHUB_OUTPUT
+go test -v ./app/operators*.go ./app/test_util.go ./app/app_types.go  
 
 echo "Flow Control" 
-go test -v ./app/flowControl*.go ./app/test_util.go ./app/app_types.go 2>&1>> $GITHUB_OUTPUT
+go test -v ./app/flowControl*.go ./app/test_util.go ./app/app_types.go
 
 echo "Strings" 
-go test -v ./app/strings*.go  ./app/test_util.go ./app/app_types.go 2>&1>> $GITHUB_OUTPUT
+go test -v ./app/strings*.go  ./app/test_util.go ./app/app_types.go
 
 echo "Slices" 
-go test -v ./app/slices*.go  ./app/test_util.go ./app/app_types.go 2>&1>> $GITHUB_OUTPUT
+go test -v ./app/slices*.go  ./app/test_util.go ./app/app_types.go
 
 echo "Binary" 
-go test -v ./app/binary*.go ./app/test_util.go ./app/app_types.go 2>&1>> $GITHUB_OUTPUT
+go test -v ./app/binary*.go ./app/test_util.go ./app/app_types.go
 
 echo "Functions" 
-go test -v ./app/functions*.go ./app/test_util.go ./app/app_types.go 2>&1>> $GITHUB_OUTPUT
+go test -v ./app/functions*.go ./app/test_util.go ./app/app_types.go
 
 echo "Methods" 
-go test -v ./app/method*.go  ./app/test_util.go ./app/app_types.go 2>&1>> $GITHUB_OUTPUT
+go test -v ./app/method*.go  ./app/test_util.go ./app/app_types.go
 
 echo "Collections" 
-go test -v ./app/collections*.go  ./app/test_util.go ./app/app_types.go 2>&1>> $GITHUB_OUTPUT
+go test -v ./app/collections*.go  ./app/test_util.go ./app/app_types.go
 
 echo "Recursion" 
-go test -v ./app/recursion*.go ./app/test_util.go ./app/app_types.go 2>&1>> $GITHUB_OUTPUT
+go test -v ./app/recursion*.go ./app/test_util.go ./app/app_types.go
 
 echo "Regexp" 
-go test -v ./app/regex*.go  ./app/test_util.go ./app/app_types.go 2>&1>> $GITHUB_OUTPUT
+go test -v ./app/regex*.go  ./app/test_util.go ./app/app_types.go
 
 echo "Async" 
-go test -v ./app/async*.go  ./app/test_util.go ./app/app_types.go 2>&1>> $GITHUB_OUTPUT
+go test -v ./app/async*.go  ./app/test_util.go ./app/app_types.go
 
 exit 0
